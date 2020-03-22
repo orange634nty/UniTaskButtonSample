219e4285df47ccfa50f769f7c131fdcf40a81985
orange634nty <orange634nty@gmail.com>
2020-03-23 00:58:09 +0900

    initial commit

---
+++ Assets/Scripts/SceneManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneManager : MonoBehaviour
+{
+    /// <summary>
+    /// use to show select popup panel
+    /// </summary>
+    [SerializeField]
+    private Button _showSelectPopupButton;
+
+    /// <summary>
+    /// display result
+    /// </summary>
+    [SerializeField]
+    private Text _selectResultText;
+
+    /// <summary>
+    /// select popup panel
+    /// </summary>
+    [SerializeField]
+    private SelectPopupPanel _selectPopupPanel;
+
+    private void Start()
+    {
+        _showSelectPopupButton.onClick.AddListener(SelectAndSetResult);
+    }
+
+    /// <summary>
+    /// show popup and update result text
+    /// </summary>
+    private async void SelectAndSetResult()
+    {
+        var result = await _selectPopupPanel.SelectChoice();
+        _selectResultText.text = $"You select {result}.";
+    }
+}
---
+++ Assets/Scripts/SelectPopupPanel.cs
@@ -0,0 +1,40 @@
+﻿using UniRx.Async;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectPopupPanel : MonoBehaviour
+{
+    /// <summary>
+    /// left button
+    /// </summary>
+    [SerializeField]
+    private Button _leftButton;
+
+    /// <summary>
+    /// right button
+    /// </summary>
+    [SerializeField]
+    private Button _rightButton;
+
+    /// <summary>
+    /// wait for user input and return result
+    /// </summary>
+    /// <returns></returns>
+    public async UniTask<string> SelectChoice()
+    {
+        // show popup
+        gameObject.SetActive(true);
+        
+        // wait for user input
+        // UniTask.WhenAny will return Task index which finished
+        var result = await UniTask.WhenAny(
+            _leftButton.OnClickAsync(),
+            _rightButton.OnClickAsync());
+
+        // delete popup
+        gameObject.SetActive(false);
+
+        // return select result
+        return result == 0 ? "left" : "right";
+    }
+}
